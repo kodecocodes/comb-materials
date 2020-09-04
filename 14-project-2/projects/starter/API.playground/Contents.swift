34e208eae9235d89d951d80c53903bcbfdf4d794
Marin Todorov <marin@underplot.com>
2020-09-04 10:22:51 +0200

    fixes warnings / updates copyright

--- 14-project-2/projects/starter/API.playground/Contents.swift
+++ 14-project-2/projects/starter/API.playground/Contents.swift
@@ -49,7 +49,7 @@ struct API {
 // Run indefinitely.
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-/// Copyright (c) 2019 Razeware LLC
+/// Copyright (c) 2019-present Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal

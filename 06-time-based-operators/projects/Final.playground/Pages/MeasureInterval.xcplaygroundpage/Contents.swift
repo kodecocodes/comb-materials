cc4e960c68a0d3727adabdaea69a9795fd134417
Florent Pillet <fpillet@gmail.com>
2020-09-13 15:52:13 +0200

    Update copyright year

--- 06-time-based-operators/projects/Final.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
+++ 06-time-based-operators/projects/Final.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
@@ -34,7 +34,7 @@ let subscription3 = measureSubject2.sink {
 
 subject.feed(with: typingHelloWorld)
 /*:
- Copyright (c) 2019 Razeware LLC
+ Copyright (c) 2020 Razeware LLC
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
--- 06-time-based-operators/projects/challenge/Final.playground/Contents.swift
+++ 06-time-based-operators/projects/challenge/Final.playground/Contents.swift
@@ -34,7 +34,7 @@ let subscription = strings
 startFeeding(subject: subject)
 
 /*:
-Copyright (c) 2019 Razeware LLC
+Copyright (c) 2020 Razeware LLC
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

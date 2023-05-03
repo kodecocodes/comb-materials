3e230171ba845f21a21f2ea0ca5434c64d9e17f9
Marin Todorov <marin@underplot.com>
2023-05-03 15:37:15 +0200

    chapter 3, 2023

--- 03-transforming-operators/projects/challenge/Starter.playground/Sources/SupportCode.swift
+++ 03-transforming-operators/projects/challenge/Starter.playground/Sources/SupportCode.swift
@@ -1,4 +1,12 @@
-/// Copyright (c) 2021 Razeware LLC
+import Foundation
+
+public func example(of description: String,
+                    action: () -> Void) {
+  print("\n——— Example of:", description, "———")
+  action()
+}
+
+/// Copyright (c) 2023 Kodeco Inc.
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -29,11 +37,3 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-
-import Foundation
-
-public func example(of description: String,
-                    action: () -> Void) {
-  print("\n——— Example of:", description, "———")
-  action()
-}

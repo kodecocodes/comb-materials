057495a2c2646bb0d3dbc804c1b533b96fdf4262
Florent Pillet <fpillet@gmail.com>
2023-06-04 19:42:10 +0200

    More copyright updates

--- 06-time-manipulation-operators/projects/Final.playground/Pages/Collect.xcplaygroundpage/Contents.swift
+++ 06-time-manipulation-operators/projects/Final.playground/Pages/Collect.xcplaygroundpage/Contents.swift
@@ -42,17 +42,17 @@ collectedPublisher2.displayEvents(in: collectedTimeline2)
 //: [Next](@next)
 /*:
  Copyright (c) 2023 Kodeco Inc.
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
  distribute, sublicense, create a derivative work, and/or sell copies of the
  Software in any work that is designed, intended, or marketed for pedagogical or
@@ -60,6 +60,10 @@ collectedPublisher2.displayEvents(in: collectedTimeline2)
  or information technology.  Permission for such use, copying, modification,
  merger, publication, distribution, sublicensing, creation of derivative works,
  or sale is expressly withheld.
+
+ This project and source code may use libraries or frameworks that are
+ released under various Open-Source licenses. Use of those libraries and
+ frameworks are governed by their own individual licenses.
  
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -69,4 +73,3 @@ collectedPublisher2.displayEvents(in: collectedTimeline2)
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
-
--- 06-time-manipulation-operators/projects/Final.playground/Pages/Timeout.xcplaygroundpage/Contents.swift
+++ 06-time-manipulation-operators/projects/Final.playground/Pages/Timeout.xcplaygroundpage/Contents.swift
@@ -27,17 +27,17 @@ timedOutSubject.displayEvents(in: timeline)
 //: [Next](@next)
 /*:
  Copyright (c) 2023 Kodeco Inc.
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
  distribute, sublicense, create a derivative work, and/or sell copies of the
  Software in any work that is designed, intended, or marketed for pedagogical or
@@ -45,6 +45,10 @@ timedOutSubject.displayEvents(in: timeline)
  or information technology.  Permission for such use, copying, modification,
  merger, publication, distribution, sublicensing, creation of derivative works,
  or sale is expressly withheld.
+
+ This project and source code may use libraries or frameworks that are
+ released under various Open-Source licenses. Use of those libraries and
+ frameworks are governed by their own individual licenses.
  
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -54,4 +58,3 @@ timedOutSubject.displayEvents(in: timeline)
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
-

2fb236db6d5ea7877b10466f96a915fc084bb47d
Florent Pillet <fpillet@gmail.com>
2023-06-04 19:42:10 +0200

    More copyright updates

--- 06-time-manipulation-operators/projects/Starter.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
+++ 06-time-manipulation-operators/projects/Starter.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
@@ -25,6 +25,10 @@ import PlaygroundSupport
  merger, publication, distribution, sublicensing, creation of derivative works,
  or sale is expressly withheld.
 
+ This project and source code may use libraries or frameworks that are
+ released under various Open-Source licenses. Use of those libraries and
+ frameworks are governed by their own individual licenses.
+ 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,4 +37,3 @@ import PlaygroundSupport
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
-
